ae7ba5a6cdd1589fb0018ff2b46c6df52f5bc6b5
David <david.naasheim@gmail.com>
2024-09-28 10:58:39 +0200

    Create Start-Pomodoro.ps1

---
+++ Start-Pomodoro.ps1
@@ -0,0 +1,30 @@
+# im missing something to break it on button press, and to log my session to a logfile of somesort. 
+# i havent looked at this https://gist.github.com/artkpv/60f1813d27d27f17431e154b9da63c59 but first hit on pomo timer powershett
+# this might be cool https://www.youtube.com/watch?v=I2lr7dakOLc havent looked at it yet. something powershell productitiy bliss pomodoro
+function Start-Pomo {
+    [CmdletBinding()]
+    param (
+        [int]$minutes = 25
+    )
+    
+    begin {
+        $seconds = $minutes * 60
+    }
+    
+    process {
+        #starting a timer
+        $processTimer = [System.Diagnostics.Stopwatch]::StartNew()
+
+
+        while ($processTimer.Elapsed.minutes -lt $minutes) {
+            Write-Progress  -SecondsRemaining ($seconds - $processTimer.Elapsed.TotalSeconds) -Activity "$($minutes-[int32]$processTimer.Elapsed.TotalMinutes) minutes left" -PercentComplete (100 / $seconds * $processTimer.Elapsed.TotalSeconds)
+            start-sleep -Seconds 1
+        }
+        $processTimer.Stop()
+
+    }
+    
+    end {
+        
+    }
+}
