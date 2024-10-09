6e1149ceb4eb59e5e885c5190ec225aaf08714fd
David <david.naasheim@gmail.com>
2024-10-09 22:39:50 +0200

    Create Get-DailyStats.ps1

---
+++ Get-DailyStats.ps1
@@ -0,0 +1,36 @@
+function Get-DailyStats {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+        $date = get-date -hour 4 -Minute 0
+        $logpath = "$env:OneDrive\StudyLog\StudyLog.json"
+        $Timespent = 0
+        $YesterDay = "$env:OneDrive\StudyLog\waniStatsyesterday.json"
+    }
+    
+    process {
+        $log = Get-Content $logpath | ConvertFrom-Json
+        $dailyStuff= Get-WaniKaniStuff -date $date
+        
+        $log.where({ $_.date -gt (get-date -hour 4 -Minute 0 ) -and $_.subject -eq "japanese" }).timespent.totalminutes.foreach({
+                $Timespent += $_
+            })
+
+
+        Write-host "-- Daily Stats --"
+        Write-Host "Timespent: $timespent minutes"
+        write-host "-- Wanikani Reviews --"
+        $dailyStuff.reviews
+        Write-Host "-- todays rank --"
+        $dailyStuff.status
+    }
+    
+    end {
+        
+    }
+}
+
+
